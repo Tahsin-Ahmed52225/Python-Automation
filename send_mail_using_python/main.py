ce2ea5699e72426e7c1f341141b33501ec810b1e
tahsin_dev <tahsinahmed52225@gmail.com>
2022-08-21 12:30:17 +0600

    Working on email sending

--- send_mail_using_python/main.py
+++ send_mail_using_python/main.py
@@ -4,4 +4,23 @@ from email.message import EmailMessage
 
 subject ="Email From Python"
 body ="Testing body from python"
-sender = "tahsindemo@gmail.com"
+sender_email = "tahsindemo@gmail.com"
+
+receiver_email = "tahsinahmed52225@gmail.com"
+password = input("Enter your password:")
+
+
+message = EmailMessage()
+message['From']=sender_email
+message['To']=receiver_email
+message['Subject']=subject
+#Adding the content body to mail
+
+
+print("Sending Email...")
+with smtplib.SMTP_SSL("smtp.gmail.com",465) as server:
+    server.ehlo()
+    server.login(sender_email,password)
+    server.sendmail(sender_email,receiver_email,message.as_string())
+    server.close()
+print("Success")
